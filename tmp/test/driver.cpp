207596792c7e911e563806584bf83b8de88136b1
elenaledo <thule@DESKTOP-NCC7R0B.localdomain>
2023-01-03 10:46:16 +0700

    pass code yay

--- tmp/test/driver.cpp
+++
@@ -1,7 +0,0 @@
-#define MAX_SIM 500
-
-void set_random(Vtop *dut, vluint64_t sim_unit) {
-  dut->operand_a_i = rand()<<1 + rand();
-  dut->operand_b_i = rand()<<1 + rand();
-  dut->alu_op_i = rand()%10;
-}
--- tmp/top.sv
+++
@@ -1,9 +0,0 @@
-module top(
-  input logic [datawidth -1:0] instrcode_i,
-  output logic  [datawidth -1:0] immgen_o
-);
-immgen dut( 
-  .instrcode_i(instrcode_i),
-  .immgen_o(immgen_o)
-  );
-endmodule: top
